323a02a16dbfa68f25290415f153141e67fcbc66
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-01-05 01:36:29 +0100

    Set up dev https server

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,13 +1,22 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { readFileSync } from 'node:fs'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  server: {
+    host: true,
+    port: 1234,
+    https:
+      process.env.NODE_ENV === 'production'
+        ? undefined
+        : {
+            key: readFileSync('./https-key.pem'),
+            cert: readFileSync('./https-certificate.pem')
+          }
+  },
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
