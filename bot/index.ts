59307df34a4bffd2395b8c514d587324292c89f1
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-01-05 01:34:00 +0100

    Add menu to onboarding

--- bot/index.ts
+++ bot/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
-import { Markup, Telegraf } from 'telegraf';
+import { Context, Markup, Telegraf } from 'telegraf';
 
 const hasProfile = (id: number) => true;
 
@@ -7,23 +7,41 @@ const WEBAPP_BASE = 'https://brachkow.com';
 
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
+const getMainMenu = (ctx: Context) =>
+  Markup.keyboard(
+    ctx.from
+      ? [
+          [
+            Markup.button.webApp(
+              'Профиль',
+              `${WEBAPP_BASE}/profiles/${ctx.from.id}`,
+            ),
+            Markup.button.webApp(
+              'Объявление',
+              `${WEBAPP_BASE}/listings/${ctx.from.id}`,
+            ),
+          ],
+          [
+            Markup.button.webApp(
+              'Мэтчи',
+              `${WEBAPP_BASE}/matches/${ctx.from.id}`,
+            ),
+          ],
+        ]
+      : [],
+  ).resize();
+
 bot.start(async (ctx) => {
   (await hasProfile(ctx.from.id))
-    ? ctx.reply('Welcome back')
+    ? ctx.reply(
+        'Здраствуйте! У вас уже есть профиль, нажмите "Профиль" чтобы посмотреть его.',
+        getMainMenu(ctx),
+      )
     : ctx.reply('Welcome');
 });
 
 bot.on('message', (ctx) => {
-  ctx.reply(
-    'Что вы хотите сделать?',
-    Markup.keyboard([
-      [
-        Markup.button.webApp('Профиль', `${WEBAPP_BASE}/profile`),
-        Markup.button.webApp('Объявление', `${WEBAPP_BASE}/listing`),
-      ],
-      [Markup.button.webApp('Мэтчи', `${WEBAPP_BASE}/matches`)],
-    ]).resize(),
-  );
+  ctx.reply('Что вы хотите сделать?', getMainMenu(ctx));
 });
 
 bot.launch();
