a1fba84132e959ab2729448ee2419608baf9659f
Timur Brachkow <33203187+brachkow@users.noreply.github.com>
2024-01-14 17:19:43 +0100

    Basic scenes

--- bot/index.ts
+++ bot/index.ts
@@ -1,11 +1,9 @@
 import 'dotenv/config';
-import { Context, Markup, Telegraf } from 'telegraf';
+import { Context, Markup, Telegraf, Scenes, session } from 'telegraf';
 import { hasProfile } from '../sdk';
 
 const BOT_TOKEN = String(process.env.BOT_TOKEN);
 const WEB_APP_URL = String(process.env.WEB_APP_URL);
-
-const bot = new Telegraf(BOT_TOKEN);
 
 const getMainMenu = (ctx: Context) =>
   Markup.keyboard(
@@ -31,21 +29,51 @@ const getMainMenu = (ctx: Context) =>
       : [],
   ).resize();
 
+const returningUserOnboardingScene = new Scenes.BaseScene<Scenes.SceneContext>(
+  'returningUserOnboarding',
+);
+returningUserOnboardingScene.enter((ctx) => {
+  console.log('Entering returningUserOnboardingScene');
+  ctx.reply(
+    'Здраствуйте! У вас уже есть профиль, нажмите "Профиль" чтобы посмотреть его.',
+    getMainMenu(ctx),
+  );
+});
+
+const newUserOnboardingScene = new Scenes.BaseScene<Scenes.SceneContext>(
+  'newUserOnboarding',
+);
+newUserOnboardingScene.enter((ctx) => {
+  console.log('Entering newUserOnboardingScene');
+  ctx.reply(
+    'Здраствуйте! У вас еще нет профиля, нажмите "Профиль" чтобы создать его.',
+    Markup.inlineKeyboard([
+      Markup.button.webApp(
+        'Создать профиль',
+        `${WEB_APP_URL}/profiles/${ctx.from.id}`,
+      ),
+    ]),
+  );
+});
+
+const bot = new Telegraf<Scenes.SceneContext>(BOT_TOKEN);
+
+const stage = new Scenes.Stage<Scenes.SceneContext>(
+  [newUserOnboardingScene, returningUserOnboardingScene],
+  {
+    ttl: 10,
+  },
+);
+
+bot.use(session());
+bot.use(stage.middleware());
+
 bot.start(async (ctx) => {
-  (await hasProfile(String(ctx.from.id)))
-    ? ctx.reply(
-        'Здраствуйте! У вас уже есть профиль, нажмите "Профиль" чтобы посмотреть его.',
-        getMainMenu(ctx),
-      )
-    : ctx.reply(
-        'Здраствуйте! У вас еще нет профиля, нажмите "Профиль" чтобы создать его.',
-        Markup.inlineKeyboard([
-          Markup.button.webApp(
-            'Создать профиль',
-            `${WEB_APP_URL}/profiles/${ctx.from.id}`,
-          ),
-        ]),
-      );
+  if (await hasProfile(String(ctx.from.id))) {
+    ctx.scene.enter('returningUserOnboarding');
+  } else {
+    ctx.scene.enter('newUserOnboarding');
+  }
 });
 
 bot.on('message', (ctx) => {
