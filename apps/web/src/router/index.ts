45f21c4a8888b076690ab0d264829f08ce9a8966
“Saventiiy” <breadduckwithpigpeppa@gmail.com>
2024-02-03 13:52:30 +0100

    Added edit profile view

--- apps/web/src/router/index.ts
+++ apps/web/src/router/index.ts
@@ -3,6 +3,7 @@ import VProfileView from '@/views/VProfileView.vue'
 import VListingView from '@/views/VListingView.vue'
 import VMatchesView from '@/views/VMatchesView.vue'
 import VRoomView from '@/views/VRoomView.vue'
+import VEditProfileView from '@/views/VEditProfileView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,7 +11,8 @@ const router = createRouter({
     { path: '/profiles/:userId', name: 'profiles', component: VProfileView },
     { path: '/listings/:userId', name: 'listings', component: VListingView },
     { path: '/matches/:userId', name: 'matches', component: VMatchesView },
-    { path: '/rooms/:userId', name: 'rooms', component: VRoomView }
+    { path: '/rooms/:userId', name: 'rooms', component: VRoomView },
+    { path: '/editProfile/:userId', name: 'editProfile', component: VEditProfileView },
   ]
 })
 
