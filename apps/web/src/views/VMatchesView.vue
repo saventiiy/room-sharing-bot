40eafe233cc835326694ed9fe29d5b2c7e100d69
“Saventiiy” <breadduckwithpigpeppa@gmail.com>
2024-02-17 18:37:24 +0100

    Added profileCardView to matchesView

--- apps/web/src/views/VMatchesView.vue
+++ apps/web/src/views/VMatchesView.vue
@@ -1,8 +1,9 @@
 <script lang="ts" setup>
   import { getMatched } from 'sdk';
-  import { Profile, Room, getAge } from 'types';
+  import { getAge } from 'types';
   import { ref, onMounted } from 'vue';
   import type { PotentialData } from 'types';
+  import VProfileCardView from './VProfileCardView.vue';
 
   const userId = useRouteParams<string>('userId');
   const dataList = ref<PotentialData[]>([]);
@@ -17,6 +18,13 @@
       console.error(err);
     }
   });
+
+  const photos = [
+    { url: "https://disgustingmen.com/wp-content/uploads/2021/06/kerouac_l_min.jpeg"},
+    { url: "https://avatars.mds.yandex.net/get-kinopoisk-image/1704946/871cba08-800e-4c77-8123-ab768799f680/220x330" },
+    { url: "https://artifex.ru/wp-content/uploads/2020/10/%D0%94%D0%B6%D0%B5%D0%BA-%D0%9A%D0%B5%D1%80%D1%83%D0%B0%D0%BA-%D0%A4%D0%BE%D1%82%D0%BE-4.jpeg"}
+  ];
+
 </script>
 
 <template>
@@ -30,42 +38,25 @@
   <div class="container">
     <div v-for="(data, index) in dataList" :key="index" class="card m-4">
       <div v-if="data.profile">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-left">
-              <figure class="image is-296x296">
-                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-              </figure>
-            </div>
-            <div class="media-content">
-              <p class="title is-4">{{ data.profile.name }}</p>
-              <p class="subtitle is-6">{{ data.profile.gender }} {{ getAge(data.profile.dateofbirth) }}</p>
-              <p class="subtitle is-6">Looking for: {{ data.profile.lookingFor }}</p>
-            </div>
-          </div>
-          <div class="content">
-            <p class="subtitle is-6"> напиши пользователю <a :href="'https://t.me/' + data.profile.username">{{ data.profile.username }}</a></p>
-            <p class="subtitle is-6">{{ data.profile.bio }}</p>
-          </div>
-        </div>
+        <VProfileCardView
+          :photos="photos"
+          :name="data.profile.name"
+          :gender="data.profile.gender"
+          :age="getAge(data.profile.dateofbirth)"
+          :lookingFor="data.profile.lookingFor"
+          :bio="data.profile.bio"
+          :isProfile="true"
+        />
       </div>
       <template v-if="data.room">
-        <div class="card-content">
-          <div class="media">
-            <div class="media-left">
-              <figure class="image is-296x296">
-                <img src="https://bulma.io/images/placeholders/96x96.png" alt="Placeholder image" />
-              </figure>
-            </div>
-            <div class="media-content">
-              <p class="title is-4">{{ data.room.address }}</p>
-              <p class="subtitle is-6">{{ data.room.district }}, {{ data.room.price }} zl</p>
-            </div>
-          </div>
-          <div class="content">
-            <p class="subtitle is-6">{{ data.room.description }}</p>
-          </div>
-        </div>
+        <VProfileCardView
+          :photos="photos"
+          :address="data.room.address"
+          :district="data.room.district"
+          :price="data.room.price"
+          :description="data.room.description"
+          :isProfile="false"
+        />
       </template>
     </div>
   </div>
