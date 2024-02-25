9ac84a0ca60b22ffe55eab4d4260049a000c7056
“Saventiiy” <breadduckwithpigpeppa@gmail.com>
2024-02-25 18:23:08 +0100

    Added sentry

--- apps/web/src/main.ts
+++ apps/web/src/main.ts
@@ -2,8 +2,22 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import 'bulma/css/bulma.css';
+import * as Sentry from "@sentry/vue";
 
 const app = createApp(App);
+
+Sentry.init({
+    dsn: "https://a2afb364c4448f96549ecce12ced4018@o4506802556502016.ingest.sentry.io/4506807530553344",
+    integrations: [
+      Sentry.browserTracingIntegration(),
+      Sentry.replayIntegration({
+        maskAllText: false,
+        blockAllMedia: false,
+      }),
+    ],
+    tracesSampleRate: 1.0,
+    replaysSessionSampleRate: 0.1,
+  });
 
 app.use(router);
 
